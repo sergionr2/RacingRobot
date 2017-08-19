1d22eabaa1bcf074166eb7e576a9326770267ca4
Antonin <antonin.raffin@ensta-paristech.fr>
2017-08-19 17:34:52 +0200

    Fix for teleop client

--- command/python/teleop_client.py
+++ command/python/teleop_client.py
@@ -8,6 +8,62 @@ import common
 
 from teleop import *
 
+def pygameMain():
+    # Pygame require a window
+    pygame.init()
+    window = pygame.display.set_mode((800,500), RESIZABLE)
+    pygame.font.init()
+    font = pygame.font.SysFont('Open Sans', 25)
+    small_font = pygame.font.SysFont('Open Sans', 20)
+    end = False
+
+    def writeText(screen, text, x, y, font, color=(62, 107, 153)):
+        text = str(text)
+        text = font.render(text, True, color)
+        screen.blit(text, (x, y))
+
+    def clear():
+        window.fill((0,0,0))
+
+    def updateScreen(window, speed, turn):
+        clear()
+        writeText(window, 'Linear: {:.2f}, Angular: {:.2f}'.format(speed, turn), 20, 0, font, (255, 255, 255))
+        help_str =  'Use arrow keys to move, q or ESCAPE to exit.'
+        writeText(window, help_str, 20, 50, small_font)
+        help_2 =  'space key, k : force stop ---  anything else : stop smoothly'
+        writeText(window, help_2, 20, 100, small_font)
+
+    x, theta, status, count = 0, 0, 0, 0
+    control_speed, control_turn = 0, 0
+    angle_order = 0
+    updateScreen(window, control_speed, control_turn)
+
+    while not end:
+        x, theta = 0, 0
+        keys = pygame.key.get_pressed()
+        for keycode in moveBindingsGame.keys():
+            if keys[keycode]:
+                x_tmp, th_tmp = moveBindingsGame[keycode]
+                x += x_tmp
+                theta += th_tmp
+
+        if keys[K_k] or keys[K_SPACE]:
+                x, theta = 0, 0
+                control_speed, control_turn = 0, 0
+
+        control_speed, control_turn = control(x, theta, control_speed, control_turn)
+        # Send Orders
+        angle_order = sendToServer(socket, control_speed, control_turn)
+
+        updateScreen(window, control_speed, angle_order)
+
+        for event in pygame.event.get():
+            if event.type == QUIT or event.type == KEYDOWN and event.key in [K_ESCAPE, K_q]:
+                end = True
+        pygame.display.flip()
+        # force 30 fps
+        pygame.time.Clock().tick(1/common.rate)
+
 # stdscr: main window
 def main(stdscr, socket):
     interface = Interface(stdscr)
@@ -37,8 +93,8 @@ def main(stdscr, socket):
         # Smooth control
         control_speed, control_turn = control(x, theta, control_speed, control_turn)
         sendToServer(socket, control_speed, control_turn)
-        # force 40 fps
-        time.sleep(1/40.0)
+        # force 10 fps
+        time.sleep(1/10.0)
 
 def sendToServer(socket, control_speed, control_turn):
     """
@@ -49,17 +105,19 @@ def sendToServer(socket, control_speed, control_turn):
     t = (control_turn + MAX_TURN) / (2 * MAX_TURN)
     angle_order = int(THETA_MIN  * t + THETA_MAX * (1 - t))
     socket.send_json((control_speed, angle_order))
+    return angle_order
 
-
+host = "192.168.12.252"
 port = "5556"
 context = zmq.Context()
 socket = context.socket(zmq.PAIR)
-socket.connect("tcp://localhost:{}".format(port))
+socket.connect("tcp://{}:{}".format(host,port))
 
 msg = socket.recv()
 print("Connected To Server")
 try:
-    curses.wrapper(main, socket)
+    # curses.wrapper(main, socket)
+    pygameMain()
 except KeyboardInterrupt as e:
     pass
 finally:
