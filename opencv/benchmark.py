f6464254a41f26ba0a5ac5ff1c4cd0d49b08f8f9
Antonin <antonin.raffin@ensta-paristech.fr>
2017-12-17 19:38:05 +0100

    Batch Prediction: x2 Speed Up!

---
+++ opencv/benchmark.py
@@ -0,0 +1,30 @@
+from __future__ import print_function, with_statement, division
+
+import time
+import argparse
+
+import cv2
+import numpy as np
+
+from opencv.image_processing import processImage
+
+N_ITER = 5000
+
+parser = argparse.ArgumentParser(description='Benchmark line detection algorithm')
+parser.add_argument('-i', '--input_image', help='Input Image', default="", type=str, required=True)
+args = parser.parse_args()
+
+
+image = cv2.imread(args.input_image)
+time_deltas = []
+for i in range(N_ITER):
+    start_time = time.time()
+    turn_percent, centroids = processImage(image, debug=False, regions=None, interactive=False)
+    time_deltas.append(time.time() - start_time)
+    # print(centroids)
+
+time_deltas = np.array(time_deltas)
+print("Total time: {:2f}s".format(time_deltas.sum()))
+print("Mean time: {:2f}s".format(time_deltas.mean()))
+print("Std time: {:2f}s".format(time_deltas.std()))
+print("Median time: {:2f}s".format(np.median(time_deltas)))
--- opencv/image_processing.py
+++ opencv/image_processing.py
@@ -75,61 +75,70 @@ def processImage(image, debug=False, regions=None, interactive=False):
     :return:(float, numpy array)
     """
     # r = [margin_left, margin_top, width, height]
-    max_width = image.shape[1]
+    im_width = image.shape[1]
     if regions is None:
         # Regions of interest
         # r = [margin_left, margin_top, width, height]
-        r0 = [0, 150, max_width, 50]
-        r1 = [0, 125, max_width, 50]
-        r2 = [0, 100, max_width, 50]
-        r3 = [0, 75, max_width, 50]
-        r4 = [0, 50, max_width, 50]
-        regions = [r1, r2, r3]  # Before [r0, r1, r2]
+        r0 = [0, 150, im_width, 50]
+        r1 = [0, 125, im_width, 50]
+        r2 = [0, 100, im_width, 50]
+        r3 = [0, 75, im_width, 50]
+        r4 = [0, 50, im_width, 50]
+        regions = np.array([r1, r2, r3]) # Before [r0, r1, r2]
 
     centroids = np.zeros((len(regions), 2), dtype=int)
     errors = [False for _ in regions]
 
-    for idx, r in enumerate(regions):
-        center = {}
-        margin_left, margin_top, _, _ = r
-        im_cropped = image[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
-
-        # TODO: batch prediction
-        im_cropped_tmp = im_cropped.copy()
-        im_width = im_cropped_tmp.shape[1]
+    if not debug:
+        # Batch Prediction
+        pred_imgs = []
         factor = im_width / WIDTH
-        pred_img = preprocessImage(im_cropped, WIDTH, HEIGHT)
-        # Predict where is the center of the line using the trained network
-        # WARNING: The scaling factor must be the same as the one used during training
-        x_center = int(pred_fn([pred_img])[0] * factor * im_width)
-        y_center = im_cropped_tmp.shape[0] // 2
+        for idx, r in enumerate(regions):
+            im_cropped = image[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+            pred_imgs.append(preprocessImage(im_cropped, WIDTH, HEIGHT))
+        centroids[:, 0] = pred_fn(np.array(pred_imgs))[:, 0] * factor * im_width
+        centroids[:, 0] += regions[:, 0]
+        centroids[:, 1] = regions[:, 3] // 2 + regions[:, 1]
+    else:
+        for idx, r in enumerate(regions):
+            center = {}
+            margin_left, margin_top, _, _ = r
+            im_cropped = image[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
 
-        if debug:
-            # Draw prediction and true center
-            cv2.circle(im_cropped_tmp, (x_center, y_center), radius=10,
-                       color=(0, 0, 255), thickness=2, lineType=8, shift=0)
-            cv2.imshow('crop_pred{}'.format(idx), im_cropped_tmp)
+            im_cropped_tmp = im_cropped.copy()
+            im_width = im_cropped_tmp.shape[1]
+            factor = im_width / WIDTH
+            pred_img = preprocessImage(im_cropped, WIDTH, HEIGHT)
+            # Predict where is the center of the line using the trained network
+            # WARNING: The scaling factor must be the same as the one used during training
+            x_center = int(pred_fn([pred_img])[0] * factor * im_width)
+            y_center = im_cropped_tmp.shape[0] // 2
 
-        if debug:
-            # display line: y = height // 2
-            h = im_cropped.shape[0] // 2
-            cv2.line(im_cropped, (0, h), (1000, h), (0, 0, 255), 2)
-            cv2.imshow('crop{}'.format(idx), im_cropped)
-            # Labeling mode
-            if interactive:
-                cv2.setMouseCallback('crop{}'.format(idx), mouseCallback, center)
-                key = cv2.waitKey(0) & 0xff
+            if debug:
+                # Draw prediction and true center
+                cv2.circle(im_cropped_tmp, (x_center, y_center), radius=10,
+                           color=(0, 0, 255), thickness=2, lineType=8, shift=0)
+                cv2.imshow('crop_pred{}'.format(idx), im_cropped_tmp)
 
-        if debug and interactive:
-            if center.get(0):
-                cx, cy = center[0]
+                # display line: y = height // 2
+                h = im_cropped.shape[0] // 2
+                cv2.line(im_cropped, (0, h), (1000, h), (0, 0, 255), 2)
+                cv2.imshow('crop{}'.format(idx), im_cropped)
+                # Labeling mode
+                if interactive:
+                    cv2.setMouseCallback('crop{}'.format(idx), mouseCallback, center)
+                    key = cv2.waitKey(0) & 0xff
+
+            if debug and interactive:
+                if center.get(0):
+                    cx, cy = center[0]
+                else:
+                    cx, cy = 0, 0
+                    errors[idx] = True
             else:
-                cx, cy = 0, 0
-                errors[idx] = True
-        else:
-            cx, cy = x_center, y_center
+                cx, cy = x_center, y_center
 
-        centroids[idx] = np.array([cx + margin_left, cy + margin_top])
+            centroids[idx] = np.array([cx + margin_left, cy + margin_top])
     # Linear Regression to fit a line
     x = centroids[:, 0]
     y = centroids[:, 1]
@@ -138,21 +147,18 @@ def processImage(image, debug=False, regions=None, interactive=False):
         pts = centroids[:2, :]
         turn_percent = 0
     else:
-        x = np.array([x[0], x[-1]])
-        y = np.array([y[0], y[-1]])
         A = np.vstack([x, np.ones(len(x))]).T
-        # TODO: use all the centroids to compute the line
-        m, b = np.linalg.lstsq(A, y)[0]
         # y = m*x + b
-        x = np.array([0, image.shape[1]], dtype=int)
-        pts = np.vstack([x, m * x + b]).T
-        pts = pts.astype(int)
+        m, b = np.linalg.lstsq(A, y)[0]
+        if debug:
+            x = np.array([0, im_width], dtype=int)
+            pts = (np.vstack([x, m * x + b]).T).astype(int)
         track_angle = np.arctan(m)
         diff_angle = abs(REF_ANGLE) - abs(track_angle)
-
+        # Estimation of the line curvature
         turn_percent = (diff_angle / MAX_ANGLE) * 100
 
-    if len(centroids) > 1:
+    if debug and len(centroids) > 1:
         a, b = pts
     else:
         pts = None
@@ -166,7 +172,7 @@ def processImage(image, debug=False, regions=None, interactive=False):
             for cx, cy in centroids:
                 cv2.circle(image, (cx, cy), radius=10, color=(0, 0, 255), thickness=1, lineType=8, shift=0)
                 cv2.line(image, tuple(a), tuple(b), color=(100, 100, 0), thickness=2, lineType=8)
-            cv2.line(image, (image.shape[1] // 2, 0), (image.shape[1] // 2, image.shape[0]),
+            cv2.line(image, (im_width // 2, 0), (im_width // 2, image.shape[0]),
                      color=(100, 0, 0), thickness=2, lineType=8)
             cv2.imshow('result', image)
 
