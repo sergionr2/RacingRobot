8b18d175ac0edfc2b118547261b92e479bb28196
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2018-05-30 16:09:20 +0200

    Add comments

--- path_planning/bezier_curve.py
+++ path_planning/bezier_curve.py
@@ -1,7 +1,7 @@
 """
-Path Planning with 4 point Beizer curve
+Path Planning with Bezier curve
 
-Author: Atsushi Sakai(@Atsushi_twi)
+Original Author: Atsushi Sakai(@Atsushi_twi)
 From https://github.com/AtsushiSakai/PythonRobotics
 """
 import scipy.special
@@ -16,6 +16,7 @@ def computeControlPoints(x, y, add_current_pos=True):
     :param x: (numpy array)
     :param y: (numpy array)
     :param add_current_pos: (bool)
+    :return: (numpy array)
     """
     control_points = np.concatenate((x[None].T, y[None].T), axis=1)
 
@@ -28,8 +29,10 @@ def computeControlPoints(x, y, add_current_pos=True):
 
 def calcBezierPath(control_points, n_points=100):
     """
+    Compute bezier path (trajectory) given control points
     :param control_points: (numpy array)
     :param n_points: (int) n_points in the trajectory
+    :return: (numpy array)
     """
     traj = []
     for t in np.linspace(0, 1, n_points):
@@ -44,6 +47,7 @@ def bernsteinPoly(n, i, t):
     :param n: (int) polynom degree
     :param i: (int)
     :param t: (float)
+    :return: (float)
     """
     return scipy.special.comb(n, i) * t ** i * (1 - t) ** (n - i)
 
@@ -53,6 +57,7 @@ def bezier(t, control_points):
     Return one point on the bezier curve
     :param t: (float)
     :param control_points: (numpy array)
+    :return: (numpy array)
     """
     n = len(control_points) - 1
     return np.sum([bernsteinPoly(n, i, t) * control_points[i] for i in range(n + 1)], axis=0)
